456dd55800af91bd510732f022acd8980f47cbdd
EbisaGirma21 <ebisagirma41@gmail.com>
2023-04-22 08:03:54 +0300

    portfolio initialization

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import Navbar from "../../Components/Navbar/Navbar"
+import "./Home.scss"
+
+function Home() {
+  return (
+    <div className="home"> 
+    <Navbar/>
+    </div>
+  )
+}
+
+export default Home
